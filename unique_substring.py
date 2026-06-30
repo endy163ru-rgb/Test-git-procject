759b7da6ec43e25b15fa904f4a4310a5eb6c42d6
stasishe13 <endy163ru@gmail.com>
2026-06-30 21:39:04 +0400

    уникальные подстроки, задание из codewars

---
+++ unique_substring.py
@@ -0,0 +1,17 @@
+def unique_substring(ss):
+    unique_string_bool = set(ss) #Он создает массив и удаляет лишние символы
+    if len(ss) == len(unique_string_bool): #Если изначальное количество символов в string и unique string не отличаются то это значит что в string нет повторяющихся символов
+        return True
+    else:
+        return False
+def len_return(s):
+    strings = []
+    for i in range(len(s)): #Переьирает по кол-ву символов в самой строке
+        for j in range(i+1,len(s)+1): #Как я понял данный слайсер должен каждый раз разбивать на слайсы все больше и больше(Отсчет как очевидно начинается с нуля). i - начало, j - конец
+            ss = s[i:j] #Обрезок для проверки в будующем
+            if unique_substring(ss):
+                strings.append(ss)
+    return len(max(strings, key=len)) #ВНИМАНИЕ key=len УЖЕ ПРОВЕРЯЕТ ПО ДЛИНЕ
+print(len_return("baacab"))
+
+    
